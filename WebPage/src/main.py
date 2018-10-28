c2017cf5ccd6b3b15fdaa4097751ff49c9590a0c
Howard Matis <HSMatis@gmail.com>
2018-10-28 11:49:25 -0700

    adding webpages

---
+++ WebPage/src/main.py
@@ -0,0 +1,169 @@
+# This program creates a web page
+# Create by Howard Matis for OpenOakland - October 19, 2018
+#
+# Takes data scraped from Oakland Legistar web page - https://oakland.legistar.com/Calendar.aspx
+#
+
+import csv
+from datetime import datetime, timedelta
+
+
+def create_html(url_open, fx):  # Read a template file and then write it main file
+    in_file = open(url_open, 'r')
+    page = in_file.read()
+    fx.write(page)
+    in_file.close()
+    return
+
+
+def read_csvfile(datafile, search_string, f2):
+    data = list(csv.reader(open(datafile), delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True))
+    numrows = len(data)
+    # numcolumns = len(data[0])
+    citycouncil = search_string
+    https = "https://"
+    for i in range(numrows):
+        if len(data[i][:]) >= 8:
+            meeting = data[i][0]
+            if citycouncil in meeting:
+                meeting_video = data[i][8]
+                meeting_date = data[i][1]
+                meeting_time = data[i][3]
+                meeting_agenda = data[i][6]
+                meeting_minutes = data[i][7]
+
+                if https in meeting_video:
+                    link = meeting_video
+                    link_text = "Click for Video Minutes"
+                elif https in meeting_minutes:
+                    link = meeting_minutes
+                    link_text = "Click for Minutes"
+                elif https in meeting_agenda:
+                    link = meeting_agenda
+                    link_text = "Agenda"
+                else:
+                    link = 'none'
+                    link_text = 'none'
+
+                present = datetime.now()
+                meeting_deadline = datetime.strptime(meeting_date, '%m/%d/%Y') + timedelta(days=1)
+                if present < meeting_deadline:
+                    link_text = "Meeting at " + meeting_time + " in the " + data[i][4]
+                    write_http_row(f2, meeting_date, link, link_text)
+                elif present >  datetime.strptime(meeting_date, '%m/%d/%Y') + timedelta(days=10):
+                    # Checking  to see if 10 days have passed since meeting.  Only keep if have video minutes
+                    if link_text == "Click for Video Minutes":
+                        write_http_row(f2, meeting_date, link, link_text)  # Need video minutes for this length of time
+                else:
+                    write_http_row(f2, meeting_date, link, link_text)  # print out meeting details in prelimary time
+
+
+def write_http_row(f2, date, link, message):
+    f2.write("\n")
+
+    lineout = "<tr>"
+    f2.write(lineout + "\n")
+
+    lineout = "<td><span class=\nstyle1\n>" + date + "</span></td>"
+    f2.write(lineout + "\n")
+
+    if link == "none":
+        lineout = '<td>' + message + '</a></td>'    #if no link omit it
+    else:
+        lineout = '<td><a href="' + link + '" target=\n_top">' + message + '</a></td>'
+    f2.write(lineout + "\n")
+
+    lineout = "</tr>"
+    f2.write(lineout + "\n")
+
+
+#
+# Write out a navigation bar
+#
+def make_navbar(list, year_list, committee_list, loop_type, loop_index, f2):
+    #   write the top of navbar
+    url = "template_navbar_top.txt"
+    create_html(url, f2)  # Create  template for HTML page
+    #
+    for index, item in enumerate(list):
+        linenav = '<li class="nav-item">'
+        f2.write(linenav + "\n")
+
+        if loop_type:
+           year_bar = str(year_list[loop_index])      # Looping over the committees so fixed year
+           committee_bar = str(index)
+        else:
+            year_bar = str(year_list[index])           # Looping over the years so fixed committee
+            committee_bar = str(loop_index)
+
+        urlnavbar = '../' + year_bar + '/committee' + committee_bar + ".html" #looping over years
+        linenav = '<a class="nav-link" href="' + urlnavbar + '"#">' + item + '</a>'
+        f2.write(linenav + "\n")
+
+        f2.write("    </li>" + "\n")
+
+    url = "template_navbar_bottom.txt"
+    create_html(url, f2)  # Create  template for HTML page
+    f2.write(" ")
+
+#
+# Main program
+#
+
+
+version = "3.0"
+print("Running Software Version", version)
+print(" ")
+
+committees = ["City Council", "Rules & Legislation", "Public Works", "Life Enrichment", "Public Safety",
+              "Oakland Redevelopment", "Community & Economic Development" , "Finance and Management"]
+years = ["2018", "2017", "2016", "2015", "2014"]
+
+for index_year, year in enumerate(years):
+    for index, committee in enumerate(committees):
+        print(year, committee)
+        outfile = "../website/" + year + "/committee" + str(index) + ".html"
+        f1 = open(outfile, 'w+')
+        #
+        #   write the top of the web page
+        url = "template_top.txt"
+        create_html(url, f1)  # Create  template for HTML page
+        f1.write(" ")
+
+        loop_committee = True # Loop over committees
+        loop_index = index_year  # Fix the year
+        make_navbar(committees, years, committees, loop_committee, loop_index, f1)
+
+        #  write the top of the web page
+        url = "template_above_table.txt"
+        create_html(url, f1)  # Create  template for HTML page
+        f1.write(" ")
+
+        line = '<div align="center"><p><h3>' + committee + " - " + year + '</h3></p></div>'
+        f1.write(line)
+
+        url = "template_table_top.txt"
+        create_html(url, f1)  # Write bottom of header file
+        f1.write(" ")
+
+        scraper_file = "../website/scraped/year" + str(year) + ".csv"
+        read_csvfile(scraper_file, committees[index], f1)
+
+        # write the bottom of the table
+        url = "template_table_bottom.txt"
+        create_html(url, f1)  # Create  template for HTML page
+        f1.write(" ")
+
+        # create the lower navigation bar
+        loop_committee = False  # Loop over years
+        loop_index = index  # Fix the year
+        make_navbar(years, years, committees, loop_committee, loop_index, f1)
+
+        # write the bottom of the web page
+        url = "template_bottom.txt"
+        create_html(url, f1)  # Create  template for HTML page
+        f1.write(" ")
+
+        f1.close()  # Close the file
+
+quit()
---
+++ WebPage/src/sidebar.py
@@ -0,0 +1,123 @@
+# This program creates a sidebar for Councilmatis
+# Create by Howard Matis for OpenOakland - October 23, 2018
+#
+# Takes data scraped from Oakland Legistar web page - https://oakland.legistar.com/Calendar.aspx
+#
+
+import csv
+from datetime import datetime, timedelta
+import calendar
+
+
+def read_csv_file(datafile, elements):
+    data = list(csv.reader(open(datafile), delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True))
+    numrows = len(data)
+    present = datetime.now()
+    today = present.strftime('%m/%d/%Y')
+
+    for i in range(numrows):        # Find out which meetings have not occurred
+        if i > 0:                   # Don't read headers
+            if len(data[i][:]) >= 8:
+                meeting_date = data[i][1]
+                future_meeting = meeting_date >= today
+                if not future_meeting:
+                    break
+                elements.append([])
+                for j in range(0, 5):
+                    elements[i-1].append(0)
+                    elements[i-1][j] = data[i][j]
+
+def write_day_header(f2, day1, day2):
+    f2.write('<div class="calendar_plan">' + "\n")
+    f2.write('<div class="cl_plan">' + "\n")
+    f2.write('<div class="cl_title"> <font size="+1">' + day1 + '</font> </div>' + "\n")
+    f2.write('<div class="cl_copy">' + day2 + '</div>' "\n")
+    f2.write('</div>' + "\n")
+    f2.write('</div>' + "\n")
+    f2.write('<br>' + "\n")
+
+def write_event_header(f2, time_event, link_calendar, name_committee, name_location):
+    f2.write('<div class="event_item">' + "\n")
+    f2.write('<div class="ei_Dot"></div>' + "\n")
+    f2.write('<div class="ei_Title">' + time_event + "\n")
+    f2.write('<a href="' + link_calendar + '“>' + "\n")
+    f2.write('<img border="0" alt="calendar link" src="../images/ical.gif" width="15" height="15"> </a></div>' + "\n")
+    f2.write('<div class="ei_Copy"> <font size="+1">' + name_committee + '</font> <br/> ' + name_location + "\n")
+    f2.write('</div>' + "\n")
+    f2.write('</div>' + "\n")
+    f2.write(' ' + "\n")
+
+
+version = "1.0"
+lookAhead = 14  # Number of the days to look ahead for meetings
+
+print("Running Software Version ", version, " - sidebar.py ")
+print(" ")
+
+outfile = "../website/html/dynamic_calendar.html"
+f1 = open(outfile, 'w+')
+
+currentDay = datetime.now().day
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
+
+# Check if close to a new year
+
+if currentMonth == 12:     # See if need to look at next year's record
+    if currentDay > 31 - lookAhead:
+        years = [str(currentYear + 1), str(currentYear)]  # Allows reading later file first
+    else:
+        years = [str(currentYear)]
+else:
+    years = [str(currentYear)]
+
+
+schedule = []
+for year in years:
+    scraper_file = "../website/scraped/year" + str(year) + ".csv"
+    print(scraper_file)
+
+    read_csv_file(scraper_file, schedule)
+
+
+numrows = len(schedule)
+
+today = datetime.now() - timedelta(days=1)
+lastdate = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+print (tomorrow)
+for i in range(numrows-1, 0, -1):
+    event_day = schedule[i][1]
+    if lastdate != event_day:
+        lastdate = event_day
+        new_day = True
+        if event_day == today:
+            day_of_week = "Today"
+        elif event_day == tomorrow:
+            day_of_week = "Tomorrow"
+        else:
+            day_datetime = datetime.strptime(event_day, '%m/%d/%Y')
+            day_label = datetime.date(day_datetime).weekday()
+            day_of_week = calendar.day_name[day_label]
+    else:
+        new_day = False
+
+    if new_day:
+        print()
+        print("New Day")
+        print(day_of_week, event_day)
+        write_day_header(f1,day_of_week, event_day)
+
+    committee = schedule[i][0]
+    if "City Council" in committee:
+        committee = "City Council - (" + committee + ")"
+    print("Meeting description")
+    print(committee)  # Committee
+    print(schedule[i][2]) # Calendar
+    print(schedule[i][3]) # Time
+    print(schedule[i][4]) # Room
+    write_event_header(f1,schedule[i][3], schedule[i][2], committee, schedule[i][4])
+
+f1.close()  # Close the file
+
+quit()
