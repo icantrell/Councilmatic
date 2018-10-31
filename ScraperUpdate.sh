b79dab3aa6c8ee5dadee10258a66895759439bbd
Howard Matis <hsmatis@gmail.com>
2018-10-31 11:11:41 -0700

    Improved script which runs daily.  Now writes a log file.  Also added a better description for video minutes.

--- ScraperUpdate.sh
+++ ScraperUpdate.sh
@@ -8,11 +8,19 @@
 # The command "date" is different depending on flavor of UNIX
 # In December the script will run the script for next year;  In January the script will run the previous year.
 #
+# This run using cron
+#   0 2 * * * /Users/matis/Documents/OpenOakland/Councilmatic-master/Councilmatic/ScraperUpdate.sh
+#
 # ------------------------------------------------------------------
 #
-cd /Users/matis/Documents/OpenOakland/Councilmatic-master/Councilmatic  #This is installation dependent
-
-
+#   This is machine dependant
+#
+DIR=/Users/matis/Documents/OpenOakland/Councilmatic-master/Councilmatic
+CRONDIR=/Users/matis/Documents/OpenOakland/Councilmatic-master/Councilmatic/WebPage/website/logs
+#
+CRONLOG=$CRONDIR/scraperupdate.log
+#
+cd $DIR
 LASTYEAR=`date -v-1y  +"%Y"`
 CURRENTYEAR=`date +"%Y"`
 NEXTYEAR=`date -v+1y  +"%Y"`
@@ -20,41 +28,44 @@ CURRENTMONTH=`date +"%m"`
 #
 #Get a list of current dates
 #
-python run_calendar.py --show_dates > temp.tmp
+echo "Version 1.0 of ScraperUpdate.sh" > $CRONLOG				#Clear cron log file
+
+python run_calendar.py --show_dates > $CRONDIR/temp.tmp
 #
 # Scrape the current year if it exists
 #
-if `grep -q "$CURRENTYEAR" "temp.tmp"`; then
-    echo "Processing Current Year Scraper File"
+if `grep -q "$CURRENTYEAR" "$CRONDIR/temp.tmp"`; then
+    echo "Processing Current Year Scraper File"  >> $CRONLOG
     python run_calendar.py -d "$CURRENTYEAR"  > WebPage/website/scraped/year"$CURRENTYEAR".csv
 fi
 #
 # Check if December
 #
 if [ "$CURRENTMONTH" == "12" ];then
-    if `grep -q "$NEXTYEAR" "temp.tmp"`; then
-        echo "December - Processing Next Year"
+    if `grep -q "$NEXTYEAR" "$CRONDIR/temp.tmp"`; then
+        echo "December - Processing Next Year"  >> $CRONLOG
         python run_calendar.py -d "$NEXTYEAR"  > WebPage/website/scraped/year"$NEXTYEAR".csv
     else
-        echo "Next year file not ready"
+        echo "Next year file not ready" >> $CRONLOG
     fi
 elif [ "$CURRENTMONTH" == "1" ];then
-    if `grep -q "$LASTYEAR" "temp.tmp"`; then
+    if `grep -q "$LASTYEAR" "$CRONDIR/temp.tmp"`; then
         echo "Current month is January - Processing Last Year"
         python run_calendar.py -d "$LASTYEAR"  > WebPage/website/scraped/year"$LASTYEAR".csv
     else
-        echo "Previous Year not available"
+        echo "Previous Year not available" >> $CRONLOG
     fi
 else
-    echo "No need to process adjacent year"
+    echo "No need to process adjacent year" >> $CRONLOG
 fi
 #
 # Now make the webpage
 #
 cd Webpage
 cd src
-python main.py   #Run the main program
-python sidebar.py #Get the sidebar
+python main.py  >> $CRONLOG #Run the main program
+python sidebar.py  >> $CRONLOG #Get the sidebar
 
-echo "Program completed"
+echo " " >> $CRONLOG
+echo "ScraperUpdate.sh completed"  >> $CRONLOG
 #
